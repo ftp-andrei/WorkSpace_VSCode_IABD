20ea84b04ae787907ef27ee486191ddf8875b94f
Andrei Miu <ftp.andreimiu@gmail.com>
2024-11-03 22:38:34 +0100

    add sql

--- BigDataAplicado/Practicas/Tema1/ConsultasModeloRelacional/pasosBD.sql
+++ BigDataAplicado/Practicas/Tema1/ConsultasModeloRelacional/pasosBD.sql
@@ -1,5 +1,6 @@
-PASO 1
+/*PASO 1*/
 -------------------
+/*
 
 docker run --name mysqlPractica -e MYSQL_ROOT_PASSWORD=secret -e MYSQL_DATABASE=Instituto -d mysql
 
@@ -15,32 +16,32 @@ show databases;
 
 use Instituto;
 
+*/
 CREATE TABLE Estudiante (
-    StudentID int NOT NULL,
+    StudentID int NOT NULL PRIMARY KEY,
     FirstName nvarchar(255),
     LastName nvarchar(255),
     DateOfBirth DATETIME,
-    CONSTRAINT PK_Estudiante PRIMARY KEY (StudentID)
+    CONSTRAINT FK_Estudiante_Curso FOREIGN KEY (CourseID) REFERENCES Curso(CourseID)
 );
 
 CREATE TABLE Curso (
-    CourseID int NOT NULL,
+    CourseID int NOT NULL PRIMARY KEY,
     CourseName nvarchar(255),
     Credits int,
-    CONSTRAINT PK_Curso PRIMARY KEY (CourseID)
+    CONSTRAINT FK_Curso_Profesor FOREIGN KEY (ProfesorID) REFERENCES Profesor(ProfesorID)
 );
 
 CREATE TABLE Profesor (
-    ProfesorID int NOT NULL,
+    ProfesorID int NOT NULL PRIMARY KEY,
     Nombre nvarchar(255),
     Apellido int,
-    CONSTRAINT PK_Profesor PRIMARY KEY (ProfesorID)
+    CONSTRAINT FK_Profesor_Departamento FOREIGN KEY (DepartmentID) REFERENCES Departamento(DepartmentID)
 );
 
 CREATE TABLE Departamento (
-    DepartmentID int NOT NULL,
-    DepartmentName nvarchar(255),
-    CONSTRAINT PK_Departamento PRIMARY KEY (DepartmentID)
+    DepartmentID int NOT NULL PRIMARY KEY,
+    DepartmentName nvarchar(255)
 );
 
 
