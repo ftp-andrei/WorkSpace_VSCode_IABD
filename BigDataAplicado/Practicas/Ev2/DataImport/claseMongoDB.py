79dcf2736de95bcad25708ffe0972a582cd6ff65
Andrei <Vespertino@C14PC01.informatica.comercio>
2025-01-10 19:36:36 +0100

    add bigdataaplicado

--- BigDataAplicado/Practicas/Ev2/DataImport/claseMongoDB.py
+++ BigDataAplicado/Practicas/Ev2/DataImport/claseMongoDB.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 import json
 
+
+
 def read_json_file(filename):
     try:
         with open(filename, 'r') as file:
@@ -8,8 +10,8 @@ def read_json_file(filename):
             return data
     except FileNotFoundError:
         return None
-    
-class MongoDBOperations:
+    # Buscar username y port
+class MongoDB:
     def __init__(self, database_name, port,username=None, password=None):
         if username and password:
             self.client = MongoClient(f'mongodb://{username}:{password}@localhost:{{port}}/')
@@ -26,12 +28,13 @@ favourite_pokemon=read_json_file("Archivos/favourite_pokemon.json")
 
 # Pasar de csv a JSON
 
-works_in_team=read_json_file("Archivos/works_in_team.csv")
-teams=read_json_file("Archivos/teams.csv")
+
 projects=read_json_file("Archivos/projects.csv")
+teams=read_json_file("Archivos/teams.csv")
+works_in_team=read_json_file("Archivos/works_in_team.csv")
 
 
-mongo_operations = MongoDBOperations('Acomodations','32769')
+mongo_operations = MongoDB('Acomodations','32769')
 
 for data in favourite_pokemon:
     mongo_operations.create_person("destinations",data)
--- BigDataAplicado/Practicas/Ev2/DataImport/consola.py
+++ BigDataAplicado/Practicas/Ev2/DataImport/consola.py
@@ -1,34 +1,51 @@
+from claseNeo4j import Neo4J
+from claseMongoDB import MongoDB
+from claseMySQL import MySQL
 
-# from mainNeo4j import Neo4jCRUD
-uri = "bolt://localhost:7687"  
-user = "neo4j"
-password = "password"
-# neo4j = Neo4jCRUD(uri, user, password)
+# Muestra el menu
+def show_menu():
+    menu= True
+    while menu:
+        print("\nMenu:")
+        print("1. Personas y sus roles en una empresa concreta\n")
+        print("2. Personas con el mismo rol en diferentes empresas.\n")
+        print("3. Empresas comunes entre dos personas.\n")
+        print("4. Personas y sus funciones en un equipo específico.\n")
+        print("5. Muestra todos los equipos con el número de personas que los componen.\n")
+        print("6. Muestra los equipos con el número total de proyectos a los que están asociados.\n")
+        print("7. Obtener todas las skills en las que una persona tiene al menos un nivel específico de proficiency.\n")
+        print("8. Encontrar todas las personas que tienen skill en al menos una skill en común con otra persona.\n")
+        print("9. Encontrar el proyecto que tenga más personas en el equipo cuyos pokemons favoritos son de diferente tipo, mostrar todo los tipos distintos (Peticion API).\n")
+        print("10. Dado una ubicación, obtén la lista de equipos que están ubicados allí junto con información de las personas que trabajan en ese equipo y los proyectos asociados.\n")
+        print("99. Exit")
+        choice = input("¿Qué desea hacer?")
         
-while True:
-    print("\nMenu:")
-    print("1. Personas y sus roles en una empresa concreta")
-    print("2. Personas con el mismo rol en diferentes empresas.")
-    print("3. Empresas comunes entre dos personas.")
-    print("4. Personas y sus funciones en un equipo específico.")
-    print("5. Muestra todos los equipos con el número de personas que los componen.")
-    print("6. Muestra los equipos con el número total de proyectos a los que están asociados.")
-    print("7. Muestra los equipos con el número total de proyectos a los que están asociados.")
-    print("8. Muestra los equipos con el número total de proyectos a los que están asociados.")
-    print("9. Muestra los equipos con el número total de proyectos a los que están asociados.")
-    choice = input("¿Qué desea hacer?")
-    
-    if choice == '1':
-        print("a")
-    elif choice == '2':
-        print("a")
-    elif choice == '3':
-        print("a")
-    elif choice == '4':
-        print("a")
-    elif choice == '5':
-        print("a")
-    elif choice == '6':
-        print("a")
-    else:
-        print("Invalid choice. Please select a valid option.")
+        if choice == '1':
+            print("a")
+        elif choice == '2':
+            print("a")
+        elif choice == '3':
+            print("a")
+        elif choice == '4':
+            print("a")
+        elif choice == '5':
+            print("a")
+        elif choice == '6':
+            print("a")
+        elif choice == '7':
+            print("a")
+        elif choice == '8':
+            print("a")
+        elif choice == '9':
+            print("a")
+        elif choice == '10':
+            print("a")           
+        elif choice == '99':
+            print("Saliendo...")
+            menu=False
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+# Llamar a la función para mostrar el menú al ejecutar el script
+if __name__ == "__main__":
+    show_menu()
